89a636d4e6e197ef07f811f33a8212fda34af6fd
ConstMarch <kostyrnd@yandex.ru>
2024-09-15 15:08:01 +0300

    lab2

---
+++ Lab_2/Task_2/Testcolors/Testcolors/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+
+namespace Testcolors
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap bitmap;
+        public Form1()
+        {
+            InitializeComponent();
+            // Загрузка и отображение изображения
+            Image image = Image.FromFile(""); 
+            bitmap = new Bitmap(image);
+
+            // Отображение изображения в каналах
+            pictureBoxTrue.Image = bitmap; 
+            pictureBoxRed.Image = ExtractChannel(bitmap, 0); // Красный канал
+            pictureBoxGreen.Image = ExtractChannel(bitmap, 1); // Зеленый канал
+            pictureBoxBlue.Image = ExtractChannel(bitmap, 2); // Синий канал
+
+            // Построение гистограмм
+            DisplayHistogram(chartRed, GetHistogram(bitmap, 'R'));
+            DisplayHistogram(chartGreen, GetHistogram(bitmap, 'G'));
+            DisplayHistogram(chartBlue, GetHistogram(bitmap, 'B'));
+        }
+
+        private Bitmap ExtractChannel(Bitmap source, int channel)
+        {
+            Bitmap channelImage = new Bitmap(source.Width, source.Height);
+
+            for (int y = 0; y < source.Height; y++)
+            {
+                for (int x = 0; x < source.Width; x++)
+                {
+                    Color pixelColor = source.GetPixel(x, y);
+                    Color newColor;
+
+                    switch (channel)
+                    {
+                        case 0: // Красный канал
+                            newColor = Color.FromArgb(pixelColor.R, 0, 0);
+                            break;
+                        case 1: // Зеленый канал
+                            newColor = Color.FromArgb(0, pixelColor.G, 0);
+                            break;
+                        case 2: // Синий канал
+                            newColor = Color.FromArgb(0, 0, pixelColor.B);
+                            break;
+                        default:
+                            newColor = Color.Black;
+                            break;
+                    }
+
+                    channelImage.SetPixel(x, y, newColor);
+                }
+            }
+
+            return channelImage;
+        }
+
+        private int[] GetHistogram(Bitmap bitmap, char channel)
+        {
+            int[] histogram = new int[256];
+
+            for (int y = 0; y < bitmap.Height; y++)
+            {
+                for (int x = 0; x < bitmap.Width; x++)
+                {
+                    Color pixelColor = bitmap.GetPixel(x, y);
+                    int value = 0;
+
+                    switch (channel)
+                    {
+                        case 'R':
+                            value = pixelColor.R;
+                            break;
+                        case 'G':
+                            value = pixelColor.G;
+                            break;
+                        case 'B':
+                            value = pixelColor.B;
+                            break;
+                    }
+
+                    histogram[value]++;
+                }
+            }
+
+            return histogram;
+        }
+
+        private void DisplayHistogram(Chart chart, int[] histogram)
+        {
+            chart.Series[0].Points.Clear();
+            for (int i = 0; i < histogram.Length; i++)
+            {
+                chart.Series[0].Points.AddXY(i, histogram[i]);
+            }
+        }
+    }
+}
---
+++ Lab_2/Task_2/Testcolors/Testcolors/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Testcolors")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Testcolors")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("39a6dade-6285-4e56-b78e-c86f19b260e0")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
