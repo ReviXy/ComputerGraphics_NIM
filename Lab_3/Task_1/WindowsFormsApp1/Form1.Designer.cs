1bc3a1c67d8f931ec7555e9fd9ac867906b925f5
Oleg <gthxbr2004@gmail.com>
2024-09-22 10:20:31 +0300

    Add 1-st task for 3-rd lab.

---
+++ Lab_3/Task_1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(867, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(95, 20);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Draw Edge";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(867, 38);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(80, 20);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Color Fill";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(867, 64);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(89, 20);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Picture Fill";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(850, 600);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Black;
+            this.pictureBox2.Location = new System.Drawing.Point(850, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(5, 600);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Black;
+            this.pictureBox3.Location = new System.Drawing.Point(0, 600);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(855, 3);
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(881, 141);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(867, 90);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(102, 20);
+            this.radioButton4.TabIndex = 7;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Select Edge";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 603);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton4;
+    }
+}
+
---
+++ Lab_3/Task_1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Drawing.Imaging;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        static string filename = "88.png";
+        static Bitmap bmFill = new Bitmap(filename);
+
+        Bitmap bm;
+        Graphics g;
+        List<Point> points;
+
+        Color penColor = Color.Black;
+        Pen pen;
+        Color fillColor = Color.Yellow;
+        Pen fillPen;
+
+        Color edgeColor = Color.DarkRed;
+
+        public Form1()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+
+            bm = new Bitmap(850, 600);
+            pictureBox1.Image = bm;
+            Clear();
+            
+            points = new List<Point>();
+            pen = new Pen(penColor);
+            fillPen = new Pen(fillColor);
+            g = Graphics.FromImage(bm);
+
+            pictureBox1.MouseDown += OnMouseDown;
+            pictureBox1.MouseMove += OnMouseMove;
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                pictureBox1.MouseDown -= ColorFill;
+                pictureBox1.MouseDown -= PictureFill;
+                pictureBox1.MouseDown -= SelectEdge;
+
+                pictureBox1.MouseDown += OnMouseDown;
+                pictureBox1.MouseMove += OnMouseMove;
+            }
+            else if (radioButton2.Checked)
+            {
+                pictureBox1.MouseDown -= OnMouseDown;
+                pictureBox1.MouseMove -= OnMouseMove;
+                pictureBox1.MouseDown -= PictureFill;
+                pictureBox1.MouseDown -= SelectEdge;
+
+                pictureBox1.MouseDown += ColorFill;
+            }
+            else if (radioButton3.Checked)
+            {
+                pictureBox1.MouseDown -= OnMouseDown;
+                pictureBox1.MouseMove -= OnMouseMove;
+                pictureBox1.MouseDown -= ColorFill;
+                pictureBox1.MouseDown -= SelectEdge;
+
+                pictureBox1.MouseDown += PictureFill;
+            }
+            else if (radioButton4.Checked)
+            {
+                pictureBox1.MouseDown -= OnMouseDown;
+                pictureBox1.MouseMove -= OnMouseMove;
+                pictureBox1.MouseDown -= ColorFill;
+                pictureBox1.MouseDown -= PictureFill;
+
+                pictureBox1.MouseDown += SelectEdge;
+            }
+        }
+
+        void ColorFill(object sender, MouseEventArgs e)
+        {
+            ColorFill_(e.Location);
+        }
+
+        private bool ColorsEqual(Color c1, Color c2) => c1.R == c2.R && c1.G == c2.G && c1.B == c2.B;
+
+        void ColorFill_(Point p)
+        {
+            if (p.X >= 0 && p.X < bm.Width && p.Y >= 0 && p.Y < bm.Height && !ColorsEqual(bm.GetPixel(p.X, p.Y), fillColor) && !ColorsEqual(bm.GetPixel(p.X, p.Y), penColor))
+            {
+                Color oldColor = bm.GetPixel(p.X, p.Y);
+                Point left = new Point(p.X, p.Y);
+                while (left.X > 0 && ColorsEqual(bm.GetPixel(left.X, left.Y), oldColor)) left.X -= 1;
+                Point right = new Point(p.X, p.Y);
+                while (right.X < bm.Width - 1 && ColorsEqual(bm.GetPixel(right.X, right.Y), oldColor)) right.X += 1;
+
+                if (left.X == 0) left.X = -1;
+                if (right.X == bm.Width - 1) right.X = bm.Width;
+
+                g.DrawLine(fillPen, left.X + 1, left.Y, right.X - 1, right.Y);
+                pictureBox1.Image = bm;
+
+                for (int i = left.X + 1; i <= right.X-1; i++)
+                    ColorFill_(new Point(i, p.Y + 1));
+                for (int i = left.X + 1; i <= right.X-1; i++)
+                    ColorFill_(new Point(i, p.Y - 1));
+            }
+        }
+
+        void PictureFill(object sender, MouseEventArgs e)
+        {
+            PictureFill_(e.Location, e.Location);
+        }
+        
+        Point GetLocalPoint(Point p, Point origin)
+        {
+            int x = bmFill.Width / 2 + (p.X - origin.X);
+            int y = bmFill.Height / 2 + (p.Y - origin.Y);
+            if (x >= bmFill.Width) x %= bmFill.Width;
+            if (y >= bmFill.Width) y %= bmFill.Height;
+            while (x < 0) x += bmFill.Width;
+            while (y < 0) y += bmFill.Height;
+            return new Point(x, y);
+        }
+
+        void PictureFill_(Point p, Point origin)
+        {
+            if (p.X >= 0 && p.X < bm.Width && p.Y >= 0 && p.Y < bm.Height && !ColorsEqual(bm.GetPixel(p.X, p.Y), penColor))
+            {
+                Color oldColor = bm.GetPixel(p.X, p.Y);
+                Point temp = GetLocalPoint(new Point(p.X, p.Y), origin);
+                if (ColorsEqual(bmFill.GetPixel(temp.X, temp.Y), oldColor)) return; // Типо проверка на уже залитый пиксель
+
+                Point left = new Point(p.X, p.Y);
+                while (left.X > 0 && ColorsEqual(bm.GetPixel(left.X, left.Y), oldColor)) left.X -= 1;
+                Point right = new Point(p.X, p.Y);
+                while (right.X < bm.Width - 1 && ColorsEqual(bm.GetPixel(right.X, right.Y), oldColor)) right.X += 1;
+
+                if (left.X == 0) left.X = -1;
+                if (right.X == bm.Width - 1) right.X = bm.Width;
+
+                // Типо заливка линии
+                for (int i = left.X + 1; i <= right.X - 1; i++)
+                {
+                    Point localP = GetLocalPoint(new Point(i, left.Y), origin);
+                    bm.SetPixel(i, left.Y, bmFill.GetPixel(localP.X, localP.Y));
+                }
+
+                pictureBox1.Image = bm;
+
+                for (int i = left.X + 1; i <= right.X - 1; i++)
+                    PictureFill_(new Point(i, p.Y + 1), origin);
+                for (int i = left.X + 1; i <= right.X - 1; i++)
+                    PictureFill_(new Point(i, p.Y - 1), origin);
+            }
+        }
+
+        Point GetNeighboor(Point p, int dir)
+        {
+            Point p1 = new Point(0, 0);
+            switch (dir)
+            {
+                case 0: p1 = new Point(p.X,      p.Y - 1); break;
+                case 1: p1 = new Point(p.X + 1,  p.Y - 1); break;
+                case 2: p1 = new Point(p.X + 1,  p.Y); break;
+                case 3: p1 = new Point(p.X + 1,  p.Y + 1); break;
+                case 4: p1 = new Point(p.X,      p.Y + 1); break;
+                case 5: p1 = new Point(p.X - 1,  p.Y + 1); break;
+                case 6: p1 = new Point(p.X - 1,  p.Y); break;
+                case 7: p1 = new Point(p.X - 1,  p.Y -1); break;
+                default:break;
+            }
+            return p1;
+        }
+
+        void SelectEdge(object sender, MouseEventArgs e)
+        {
+            LinkedList<Point> edge = SelectEdge_(e.Location);
+            if (edge.Count > 0)
+            {
+                foreach (Point p in edge)
+                    bm.SetPixel(p.X, p.Y, edgeColor);
+                pictureBox1.Image = bm;
+            }
+        }
+
+        LinkedList<Point> SelectEdge_(Point start)
+        {
+            LinkedList<Point> edge = new LinkedList<Point>();
+            edge.AddLast(start);
+            Color c = bm.GetPixel(start.X, start.Y);
+
+            Point cur = start;
+            int dir = 4;
+            
+            while (true)
+            {
+                dir += 2;
+                if (dir > 7) dir -= 8;
+                Point p;
+
+                for (int i = 0; i < 8; i++)
+                {
+                    p = GetNeighboor(cur, dir);
+                    if (!(p.X >= 0 && p.X < bm.Width && p.Y >= 0 && p.Y < bm.Height)) continue;
+                    if (ColorsEqual(bm.GetPixel(p.X, p.Y), c)) goto a;
+                    dir--;
+                    if (dir < 0) dir += 8;
+                }
+                return new LinkedList<Point>();
+            a:
+                if (p.X == start.X && p.Y == start.Y) break;
+
+                edge.AddLast(p);
+                cur = p;
+            }
+            return edge;
+        }
+
+        void OnMouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left)
+                return;
+
+            points.Clear();
+            points.Add(e.Location);
+        }
+
+
+        void OnMouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left)
+                return;
+
+            points.Add(e.Location);
+
+            if (points.Count < 2)
+                return;
+
+            g.DrawLines(pen, points.ToArray());
+            pictureBox1.Image = bm;
+        }
+
+        private void Clear()
+        {
+            var g = Graphics.FromImage(pictureBox1.Image);
+            g.Clear(pictureBox1.BackColor);
+            pictureBox1.Image = pictureBox1.Image;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+    }
+}
