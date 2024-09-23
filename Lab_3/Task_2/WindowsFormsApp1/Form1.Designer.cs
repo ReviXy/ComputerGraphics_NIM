eb16c8551ce6514d81cb6bef477d1e02777147e1
ConstMarch <kostyrnd@yandex.ru>
2024-09-23 22:00:12 +0300

    lab3 task2

---
+++ Lab_3/Task_2/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(650, 10);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(78, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Bresenham";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(650, 31);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(42, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Wu";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(638, 488);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            //this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Black;
+            this.pictureBox2.Location = new System.Drawing.Point(638, 0);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(4, 488);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Black;
+            this.pictureBox3.Location = new System.Drawing.Point(0, 488);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(641, 2);
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(658, 52);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 19);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(736, 490);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Lab_3/Task_2/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Drawing.Imaging;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bm;
+        Graphics g;
+        List<Point> points;
+        const int MAX_COLOR_VALUE = 255;
+        Color penColor = Color.Black;
+        Pen pen;
+        public Form1()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+
+            bm = new Bitmap(850, 600);
+            pictureBox1.Image = bm;
+            Clear();
+            
+            points = new List<Point>();
+            pen = new Pen(penColor);
+            g = Graphics.FromImage(bm);
+            pictureBox1.Click += pictureBox1_Click_1;
+
+        }
+        void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+            Point point = pictureBox1.PointToClient(Cursor.Position);
+            using (Graphics g = pictureBox1.CreateGraphics())
+                if (points.Count < 2 && !points.Contains(point))
+                {
+                    g.FillRectangle(Brushes.Black, point.X, point.Y, 2, 2);
+                    points.Add(point);
+                }
+            if (radioButton1.Checked && (points.Count ==2))
+            {
+                int x0 = points[points.Count - 2].X;
+                int y0 = points[points.Count - 2].Y;
+                int x1 = points[points.Count-1].X;
+                int y1 = points[points.Count-1].Y;
+                Bresenham(x0, y0, x1, y1);
+                pictureBox1.Image = bm;
+            }
+            if (radioButton2.Checked && (points.Count ==2))
+            {
+                int x0 = points[points.Count - 2].X;
+                int y0 = points[points.Count - 2].Y;
+                int x1 = points[points.Count-1].X;
+                int y1 = points[points.Count-1].Y;
+               Wu(x0, y0, x1, y1);
+                pictureBox1.Image = bm;
+            }
+        }
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                points.Clear();
+                Clear();
+                pictureBox1.Click += pictureBox1_Click_1;
+            }
+            else if (radioButton2.Checked)
+            {
+                points.Clear();
+                Clear();
+                pictureBox1.Click += pictureBox1_Click_1;
+            }
+        }
+        void Bresenham(int xstart, int ystart, int xend, int yend)
+        {
+            if (xstart > xend)
+            {
+                Swap(ref xstart, ref xend);
+                Swap(ref ystart, ref yend);
+            }
+            int dx = xend - xstart;
+            int dy = yend - ystart;
+            int xi = xstart;
+            int yi = ystart;
+            int step = 1;
+            int di = 2 * (dy - dx);
+            if (dx == 0 || Math.Abs(dy / (double)dx) > 1)
+            {
+                if (dy / (double)dx < 0)
+                {
+                    xi = xend;
+                    step = -1;
+                    dy = -dy;
+                    int temp = ystart;
+                    ystart = yend;
+                    yend = temp;
+                }
+                for (yi = ystart; yi <= yend; yi++)
+                {
+                    bm.SetPixel(xi, yi, Color.Black);
+                    if (di >= 0)
+                    {
+                        xi += step;
+                        di += 2 * (dx - dy);
+                    }
+                    else
+                    {
+                        di += 2 * dx;
+                    }
+                }
+            }
+            else
+            {
+                if (dy / (double)dx < 0)
+                {
+                    step = -1;
+                    dy = -dy;
+                }
+                for (xi = xstart; xi <= xend; xi++)
+                {
+                    bm.SetPixel(xi, yi, Color.Black);
+                    if (di >= 0)
+                    {
+                        yi += step;
+                        di += 2 * (dy - dx);
+                    }
+                    else
+                    {
+                        di += 2 * dy;
+                    }
+                }
+            }
+
+        }
+
+
+        void Wu(int xstart, int ystart, int xend, int yend)
+        {
+            if (xstart > xend)
+            {
+                Swap(ref xstart, ref xend);
+                Swap(ref ystart, ref yend);
+            }
+
+            int dx = xend - xstart;
+            int dy = yend - ystart;
+            double gradient;
+            if (dx == 0)
+            {
+                gradient = 1;
+            }
+            else if (dy == 0)
+            {
+                gradient = 0;
+            }
+            else
+            {
+                gradient = dy / (double)dx;
+            }
+
+            int step = 1;
+            double xi = xstart;
+            double yi = ystart;
+
+            if (Math.Abs(gradient) > 1)
+            {
+                gradient = 1 / gradient;
+                if (gradient < 0)
+                {
+                    xi = xend;
+                    step = -1;
+                    int temp = ystart;
+                    ystart = yend;
+                    yend = temp;
+                }
+
+                for (yi = ystart; yi <= yend; yi += 1)
+                {
+                    int help;
+                    if (gradient < 0)
+                    {
+                        help = (int)(MAX_COLOR_VALUE * (xi - (int)xi));
+                    }
+                    else
+                    {
+                        help = MAX_COLOR_VALUE - (int)(MAX_COLOR_VALUE * (xi - (int)xi));
+                    }
+                    bm.SetPixel((int)xi, (int)yi, Color.FromArgb(MAX_COLOR_VALUE - help, MAX_COLOR_VALUE - help, MAX_COLOR_VALUE - help));
+                    bm.SetPixel((int)xi + step, (int)yi, Color.FromArgb(help, help, help));
+                    xi += gradient;
+                }
+            }
+            else
+            {
+                if (gradient < 0)
+                {
+                    step = -1;
+                }
+                for (xi = xstart; xi <= xend; xi += 1)
+                {
+                    int help;
+                    if (gradient < 0)
+                    {
+                        help = (int)(MAX_COLOR_VALUE * (yi - (int)yi));
+                    }
+                    else
+                    {
+                        help = MAX_COLOR_VALUE - (int)(MAX_COLOR_VALUE * (yi - (int)yi));
+                    }
+                    bm.SetPixel((int)xi, (int)yi, Color.FromArgb(MAX_COLOR_VALUE - help, MAX_COLOR_VALUE - help, MAX_COLOR_VALUE - help));
+                    bm.SetPixel((int)xi, (int)yi + step, Color.FromArgb(help, help, help));
+                    yi += gradient;
+                }
+            }
+        }
+        void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+
+        private void Clear()
+        {
+            var g = Graphics.FromImage(pictureBox1.Image);
+            g.Clear(pictureBox1.BackColor);
+            pictureBox1.Image = pictureBox1.Image;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            points.Clear();
+            Clear();
+        }
+
+    }
+}
