a5adf58895001f25f121de492dc81f255cc5128b
ConstMarch <kostyrnd@yandex.ru>
2024-09-29 18:50:39 +0300

    move, rotate and ui update

--- Lab_4/WindowsFormsApp1/Form1.cs
+++ Lab_4/WindowsFormsApp1/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -140,7 +142,6 @@ namespace WindowsFormsApp1
                         outputTextBox.Text = "Выберите полигон и угол поворота. После чего нажмите Apply.";
                     }
                     break;
-
                 default: break;
             }
         }
@@ -157,11 +158,8 @@ namespace WindowsFormsApp1
                         outputTextBox.Text = "Ошибка входных данных";
                         return;
                     }
-
-                    // To do Хачу матрицы
                     for (int i = 0; i < polygons[polygonSelectDropDown.SelectedIndex - 1].Count; i++)
-                        polygons[polygonSelectDropDown.SelectedIndex - 1][i] = new Point(polygons[polygonSelectDropDown.SelectedIndex - 1][i].X + dx, polygons[polygonSelectDropDown.SelectedIndex - 1][i].Y + dy);
-
+                        polygons[polygonSelectDropDown.SelectedIndex - 1][i] = Movepoint(polygons[polygonSelectDropDown.SelectedIndex - 1][i], dx, dy);
                     mode = Mode.Idle;
                     clearButton.Enabled = true;
                     SetModeButtons(true);
@@ -177,16 +175,14 @@ namespace WindowsFormsApp1
                     break;
 
                 case Mode.Turn_Around_Point:
-                    float turn = 0;
-                    if (polygonSelectDropDown.SelectedIndex == 0 || !float.TryParse(inputTextBox.Text, out turn))
+                    int turn = 0;
+                    if (polygonSelectDropDown.SelectedIndex == 0 || !int.TryParse(inputTextBox.Text, out turn))
                     {
                         outputTextBox.Text = "Ошибка входных данных";
                         return;
                     }
-
-                    // To do Хачу преобразование
-                    // Хачу использование tempPoint
-
+                    for (int i = 0; i < polygons[polygonSelectDropDown.SelectedIndex - 1].Count; i++)
+                        polygons[polygonSelectDropDown.SelectedIndex - 1][i]=RotatePoint(polygons[polygonSelectDropDown.SelectedIndex - 1][i],tempPoint,turn);
                     mode = Mode.Idle;
                     clearButton.Enabled = true;
                     SetModeButtons(true);
@@ -199,7 +195,34 @@ namespace WindowsFormsApp1
                     outputTextBox.Text = "";
 
                     DrawPolygons();
+                    pointFlag = true;
+                break;
+
+                case Mode.Turn_Around_Center:
+                    if (polygonSelectDropDown.SelectedIndex == 0 || !int.TryParse(inputTextBox.Text, out turn))
+                    {
+                        outputTextBox.Text = "Ошибка входных данных";
+                        return;
+                    }
+                    Point tempP = new Point();
+                    tempP.X=PolygonCentre(polygons[polygonSelectDropDown.SelectedIndex - 1]).X;
+                    tempP.Y= PolygonCentre(polygons[polygonSelectDropDown.SelectedIndex - 1]).Y;
+                    for (int i = 0; i < polygons[polygonSelectDropDown.SelectedIndex - 1].Count; i++)
+                        polygons[polygonSelectDropDown.SelectedIndex - 1][i] = RotatePoint(polygons[polygonSelectDropDown.SelectedIndex - 1][i], tempP, turn);
+                    clearButton.Enabled = true;
+                    SetModeButtons(true);
+
+                    polygonSelectDropDown.SelectedIndex = 0;
+                    polygonSelectDropDown.Enabled = false;
+                    inputTextBox.Text = "";
+                    inputTextBox.Enabled = false;
+                    applyButton.Enabled = false;
+                    outputTextBox.Text = "";
+
+                    DrawPolygons();
+                    pointFlag = true;
                     break;
+
                 default: break;
             }
         }
@@ -213,27 +236,61 @@ namespace WindowsFormsApp1
 
         private void movePolygonButton_Click(object sender, EventArgs e)
         {
-            mode = Mode.Move_Polygon;
-            clearButton.Enabled = false;
-            SetModeButtons(false);
+            if (polygons.Count > 0)
+            {
+                mode = Mode.Move_Polygon;
+                clearButton.Enabled = false;
+                SetModeButtons(false);
 
-            polygonSelectDropDown.Enabled = true;
-            inputTextBox.Enabled = true;
-            applyButton.Enabled = true;
-            outputTextBox.Text = "Выберите полигон и введите целые dx и dy через пробел. После чего нажмите Apply.";
+                polygonSelectDropDown.Enabled = true;
+                inputTextBox.Enabled = true;
+                applyButton.Enabled = true;
+                outputTextBox.Text = "Выберите полигон и введите целые dx и dy через пробел. После чего нажмите Apply.";
+            }
+            else
+            {
+                outputTextBox.Text = "Нет полигонов";
+                clearButton.Enabled = true;
+                SetModeButtons(true);
+            }
         }
 
         private void turnAroundPointButton_Click(object sender, EventArgs e)
         {
-            mode = Mode.Turn_Around_Point;
-            clearButton.Enabled = false;
-            SetModeButtons(false);
+            if (polygons.Count > 0)
+            {
+                mode = Mode.Turn_Around_Point;
+                clearButton.Enabled = false;
+                SetModeButtons(false);
 
-            outputTextBox.Text = "Выберите точку, относительно которой будет выполняться поворот.";
+                outputTextBox.Text = "Выберите полигон и точку, относительно которой будет выполняться поворот.После чего нажмите Apply";
+            }
+            else
+            {
+                outputTextBox.Text = "Нет полигонов";
+                clearButton.Enabled = true;
+                SetModeButtons(true);
+            }
         }
 
         private void turnAroundCenterButton_Click(object sender, EventArgs e)
         {
+            mode = Mode.Turn_Around_Center;
+            clearButton.Enabled = false;
+            SetModeButtons(false);
+            if (polygons.Count > 0)
+            {
+                polygonSelectDropDown.Enabled = true;
+                inputTextBox.Enabled = true;
+                applyButton.Enabled = true;
+                outputTextBox.Text = "Выберите полигон, относительно которой будет выполняться поворот.После чего нажмите Apply";
+            }
+            else
+            {
+                outputTextBox.Text = "Нет полигонов";
+                clearButton.Enabled = true;
+                SetModeButtons(true);
+            }
 
         }
 
@@ -285,7 +342,81 @@ namespace WindowsFormsApp1
         {
             Clear();
         }
+        private int[] Multiplyint(int[][] Matrix, int[] array)
+        {
+            int[] resultVector = new int[3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                    resultVector[i] += Matrix[j][i] * array[j];
+            }
+            return resultVector;
+        }
+        private double[] Multiplydouble(double[][] Matrix, int[] array)
+        {
+            double[] resultVector = new double[3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                    resultVector[i] += Matrix[j][i] * array[j];
+            }
+            return resultVector;
+        }
+        private Point Movepoint(Point polygonpoint, int dx, int dy)
+        {
+            int[][] Matrix = new int[3][]
+            {
+                    new int[3] { 1,   0, 0 },
+                    new int[3] { 0,   1, 0 },
+                    new int[3] { dx, dy, 1 }
+            };
+            int[] offsetVector = new int[3] { polygonpoint.X, polygonpoint.Y, 1 };
+            int[] resultVector = Multiplyint(Matrix, offsetVector);
+            return new Point((int)resultVector[0], (int)resultVector[1]);
+        }
+        private Point RotatePoint(Point polygonpoint, Point PointofRotate, int rotateAngle)
+        {
+            double pointA, pointB;
+            double angle = (rotateAngle / 180D) * Math.PI;
 
-        
+            pointA = -PointofRotate.X * Math.Cos(angle) + PointofRotate.Y * Math.Sin(angle) + PointofRotate.X;
+            pointB = -PointofRotate.X * Math.Sin(angle) - PointofRotate.Y * Math.Cos(angle) + PointofRotate.Y;
+
+
+
+            int[] offsetVector = new int[3] { polygonpoint.X, polygonpoint.Y, 1 };
+            double[][] Matrix = new double[3][]
+            {
+                new double[3] {  Math.Cos(angle),   Math.Sin(angle), 0 },
+                new double[3] { -Math.Sin(angle),   Math.Cos(angle), 0 },
+                new double[3] { pointA, pointB, 1 } 
+            };
+            double[] resultVector = Multiplydouble(Matrix, offsetVector);
+            return new Point((int)resultVector[0], (int)resultVector[1]);
+        }
+        private Point PolygonCentre(List<Point> polygon)
+        {
+            int minx= polygon[0].X,maxx= polygon[0].X,miny = polygon[0].Y,maxy = polygon[0].Y;
+            for (int i = 0; i < polygon.Count - 1; i++)
+            {
+                if (polygon[i].X< minx)
+                {
+                    minx = polygon[i].X;
+                }
+                if (polygon[i].X>maxx)
+                {
+                    maxx=polygon[i].X;
+                }
+                if (polygon[i].Y<miny)
+                {
+                    miny= polygon[i].Y;
+                }
+                if (polygon[i].Y>maxy)
+                {
+                    maxy = polygon[i].Y;
+                }
+            }
+            return new Point((maxx + minx) / 2, (maxy + miny) / 2);
+        }
     }
 }
