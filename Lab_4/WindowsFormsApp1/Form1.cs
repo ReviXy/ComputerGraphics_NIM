c31aaf053a567d6240ba342a8669fbcb64429ed5
Oleg <gthxbr2004@gmail.com>
2024-09-30 01:08:30 +0300

    Add 3 last tasks for 4-th lab. Minor fixes.

--- Lab_4/WindowsFormsApp1/Form1.cs
+++ Lab_4/WindowsFormsApp1/Form1.cs
@@ -49,6 +49,7 @@ namespace WindowsFormsApp1
             pictureBox.Image = bm;
             g = Graphics.FromImage(pictureBox.Image);
             tempPoint = new Point(); // Потому что больше негде
+            tempEdge = new List<Point>(); // Потому что больше негде
 
             polygonSelectDropDown.Items.Clear();
             polygonSelectDropDown.Items.Add("");
@@ -103,8 +104,10 @@ namespace WindowsFormsApp1
             pictureBox.Image = bm;
         }
 
+        int intFlag = 0;
         bool pointFlag = true;
         Point tempPoint;
+        List<Point> tempEdge;
 
         void OnMouseDown(object sender, MouseEventArgs e)
         {
@@ -142,6 +145,135 @@ namespace WindowsFormsApp1
                         outputTextBox.Text = "Выберите полигон и угол поворота. После чего нажмите Apply.";
                     }
                     break;
+
+                case Mode.Position_Relative_To_Edge:
+                    switch (intFlag)
+                    {
+                        case 0: 
+                            if (!tempEdge.Contains(e.Location))
+                            {
+                                tempEdge.Add(e.Location);
+                                bm.SetPixel(e.Location.X, e.Location.Y, polygonColor);
+
+                                if(tempEdge.Count == 2)
+                                {
+                                    intFlag = 1;
+                                    g.DrawLine(new Pen(selectedPolygonColor), tempEdge[0], tempEdge[1]);
+                                    outputTextBox.Text = "Выберите точку.";
+                                }
+                                pictureBox.Image = bm;
+                            }
+                            break;
+                        case 1:
+                            tempPoint = e.Location;
+                            bm.SetPixel(e.Location.X, e.Location.Y, selectedPolygonColor);
+                            pictureBox.Image = bm;
+                            intFlag = 2;
+
+                            Point b = new Point (tempPoint.X - tempEdge[0].X, tempPoint.Y - tempEdge[0].Y);
+                            Point a = new Point(tempEdge[1].X - tempEdge[0].X, tempEdge[1].Y - tempEdge[0].Y);
+                            float res = b.X * a.Y - b.Y * a.X;
+                            string str;
+                            if (res > 0) str = "Точка слева от ребра.";
+                            else if (res < 0) str = "Точка справа от ребра.";
+                            else str = "Точка на ребре.";
+                            str += "Нажмите на экран, чтобы продолжить.";
+                            outputTextBox.Text = str;
+
+                            break;
+                        case 2:
+                            mode = Mode.Idle;
+                            intFlag = 0;
+                            clearButton.Enabled = true;
+                            SetModeButtons(true);
+
+                            polygonSelectDropDown.SelectedIndex = 0;
+                            inputTextBox.Text = "";
+                            outputTextBox.Text = "";
+
+                            DrawPolygons();
+                            break;
+                        default:break;
+                    }
+                    break;
+
+                case Mode.Convexity_Check:
+                    if (intFlag == 0)
+                    {
+                        tempPoint = e.Location;
+                        bm.SetPixel(e.Location.X, e.Location.Y, selectedPolygonColor);
+                        pictureBox.Image = bm;
+                        intFlag = 1;
+
+                        polygonSelectDropDown.Enabled = true;
+                        applyButton.Enabled = true;
+                        outputTextBox.Text = "Выберите полигон. После чего нажмите Apply.";
+                    }
+                    else if (intFlag == 2)
+                    {
+                        mode = Mode.Idle;
+                        intFlag = 0;
+                        clearButton.Enabled = true;
+                        SetModeButtons(true);
+
+                        polygonSelectDropDown.SelectedIndex = 0;
+                        inputTextBox.Text = "";
+                        outputTextBox.Text = "";
+
+                        DrawPolygons();
+                    }
+                    break;
+
+                case Mode.Find_Intersection:
+                    if (intFlag < 4)
+                    {
+                        tempEdge.Add(e.Location);
+                        bm.SetPixel(e.Location.X, e.Location.Y, polygonColor);
+                        intFlag++;
+                        if (tempEdge.Count % 2 == 0)
+                        {
+                            
+                            g.DrawLine(new Pen(polygonColor), tempEdge[tempEdge.Count - 2], tempEdge[tempEdge.Count - 1]);
+                        }
+                        pictureBox.Image = bm;
+                    }
+                    if (intFlag == 4)
+                    {
+                        Point a = tempEdge[0];
+                        Point b = tempEdge[1];
+                        Point c = tempEdge[2];
+                        Point d = tempEdge[3];
+                        Point n = new Point(-(d.Y - c.Y), d.X - c.X);
+
+                        float t = -(float)(n.X * (a.X - c.X) + n.Y * (a.Y - c.Y)) / (n.X * (b.X - a.X) + n.Y * (b.Y - a.Y));
+
+                        if (n.X * (b.X - a.X) + n.Y * (b.Y - a.Y) == 0) { outputTextBox.Text = "Прямые параллельны."; goto qwe; }
+
+                        Point res = new Point(a.X + (int)(t * (b.X - a.X)), a.Y + (int)(t * (b.Y - a.Y)));
+                        g.DrawRectangle(new Pen(selectedPolygonColor), res.X, res.Y, 1, 1);
+                        pictureBox.Image = bm;
+
+                        if (res.X >= Math.Min(a.X, b.X) && res.X <= Math.Max(a.X, b.X) && res.Y >= Math.Min(a.Y, b.Y) && res.Y <= Math.Max(a.Y, b.Y)
+                            && res.X >= Math.Min(c.X, d.X) && res.X <= Math.Max(c.X, d.X) && res.Y >= Math.Min(c.Y, d.Y) && res.Y <= Math.Max(c.Y, d.Y)) 
+                            outputTextBox.Text = $"Точка пересечения - ({res.X}, {res.Y}). Нажмите на экран, чтобы продолжить.";
+                        else outputTextBox.Text = $"Рёбра не пересекаются.";
+                        qwe:
+                        intFlag++;
+                    }
+                    else if (intFlag == 5)
+                    {
+                        mode = Mode.Idle;
+                        intFlag = 0;
+                        clearButton.Enabled = true;
+                        SetModeButtons(true);
+
+                        polygonSelectDropDown.SelectedIndex = 0;
+                        inputTextBox.Text = "";
+                        outputTextBox.Text = "";
+
+                        DrawPolygons();
+                    }
+                    break;
                 default: break;
             }
         }
@@ -158,8 +290,10 @@ namespace WindowsFormsApp1
                         outputTextBox.Text = "Ошибка входных данных";
                         return;
                     }
+
                     for (int i = 0; i < polygons[polygonSelectDropDown.SelectedIndex - 1].Count; i++)
                         polygons[polygonSelectDropDown.SelectedIndex - 1][i] = Movepoint(polygons[polygonSelectDropDown.SelectedIndex - 1][i], dx, dy);
+
                     mode = Mode.Idle;
                     clearButton.Enabled = true;
                     SetModeButtons(true);
@@ -205,8 +339,8 @@ namespace WindowsFormsApp1
                         return;
                     }
                     Point tempP = new Point();
-                    tempP.X=PolygonCentre(polygons[polygonSelectDropDown.SelectedIndex - 1]).X;
-                    tempP.Y= PolygonCentre(polygons[polygonSelectDropDown.SelectedIndex - 1]).Y;
+                    tempP.X = PolygonCentre(polygons[polygonSelectDropDown.SelectedIndex - 1]).X;
+                    tempP.Y = PolygonCentre(polygons[polygonSelectDropDown.SelectedIndex - 1]).Y;
                     for (int i = 0; i < polygons[polygonSelectDropDown.SelectedIndex - 1].Count; i++)
                         polygons[polygonSelectDropDown.SelectedIndex - 1][i] = RotatePoint(polygons[polygonSelectDropDown.SelectedIndex - 1][i], tempP, turn);
                     clearButton.Enabled = true;
@@ -219,8 +353,63 @@ namespace WindowsFormsApp1
                     applyButton.Enabled = false;
                     outputTextBox.Text = "";
 
+                    outputTextBox.Text = "";
+
                     DrawPolygons();
                     pointFlag = true;
+                    break;
+
+                case Mode.Convexity_Check:
+                    if (polygonSelectDropDown.SelectedIndex == 0)
+                    {
+                        outputTextBox.Text = "Ошибка входных данных";
+                        return;
+                    }
+
+                    int cnt = 0;
+                    List<Point> l = new List<Point>(polygons[polygonSelectDropDown.SelectedIndex - 1]);
+                    if (l.Count > 2)
+                    {
+                        for (int i = 1; i <= l.Count; i++)
+                        {
+                            Point a = tempPoint;
+                            Point b = new Point(tempPoint.X + 1, tempPoint.Y);
+                            Point c = l[i - 1];
+                            Point d = l[i % l.Count];
+                            Point n = new Point(-(d.Y - c.Y), d.X - c.X);
+                            float t = -(float)(n.X * (a.X - c.X) + n.Y * (a.Y - c.Y)) / (n.X * (b.X - a.X) + n.Y * (b.Y - a.Y));
+                            Point res = new Point(a.X + (int)(t * (b.X - a.X)), a.Y + (int)(t * (b.Y - a.Y)));
+                            if (res.X >= Math.Min(c.X, d.X) && res.X <= Math.Max(c.X, d.X) && res.X > a.X) cnt++;
+
+                        }
+                    }
+
+                    if (cnt % 2 == 0)
+                        outputTextBox.Text = "Точка не лежит внутри полигона. Нажмите на экран, чтобы продолжить.";
+                    else
+                    {
+                        Point b = new Point(tempPoint.X - l[0].X, tempPoint.Y - l[0].Y);
+                        Point a = new Point(l[1].X - l[0].X, l[1].Y - l[0].Y);
+                        bool sign = b.X * a.Y - b.Y * a.X > 0;
+
+                        bool res = true;
+
+                        for (int i = 1; i <= l.Count; i++)
+                        {
+                            b = new Point(tempPoint.X - l[i - 1].X, tempPoint.Y - l[i - 1].Y);
+                            a = new Point(l[i % l.Count].X - l[i - 1].X, l[i % l.Count].Y - l[i - 1].Y);
+                            float t1 = -(b.Y * a.X - b.X * a.Y);
+                            if (!(sign && t1 > 0 || !sign && t1 < 0)) { res = false; break; }
+                        }
+
+                        if (res) outputTextBox.Text = "Точка лежит внутри выпуклого полигона.";
+                        else outputTextBox.Text = "Точка лежит внутри вогнутого полигона.";
+                    }
+
+                    polygonSelectDropDown.Enabled = false;
+                    applyButton.Enabled = false;
+
+                    intFlag = 2;
                     break;
 
                 default: break;
@@ -250,8 +439,6 @@ namespace WindowsFormsApp1
             else
             {
                 outputTextBox.Text = "Нет полигонов";
-                clearButton.Enabled = true;
-                SetModeButtons(true);
             }
         }
 
@@ -263,33 +450,29 @@ namespace WindowsFormsApp1
                 clearButton.Enabled = false;
                 SetModeButtons(false);
 
-                outputTextBox.Text = "Выберите полигон и точку, относительно которой будет выполняться поворот.После чего нажмите Apply";
+                outputTextBox.Text = "Выберите точку, относительно которой будет выполняться поворот.";
             }
             else
             {
                 outputTextBox.Text = "Нет полигонов";
-                clearButton.Enabled = true;
-                SetModeButtons(true);
             }
         }
 
         private void turnAroundCenterButton_Click(object sender, EventArgs e)
         {
-            mode = Mode.Turn_Around_Center;
-            clearButton.Enabled = false;
-            SetModeButtons(false);
             if (polygons.Count > 0)
             {
+                mode = Mode.Turn_Around_Center;
+                clearButton.Enabled = false;
+                SetModeButtons(false);
                 polygonSelectDropDown.Enabled = true;
                 inputTextBox.Enabled = true;
                 applyButton.Enabled = true;
-                outputTextBox.Text = "Выберите полигон, относительно которой будет выполняться поворот.После чего нажмите Apply";
+                outputTextBox.Text = "Выберите полигон и угол поворота. После чего нажмите Apply";
             }
             else
             {
                 outputTextBox.Text = "Нет полигонов";
-                clearButton.Enabled = true;
-                SetModeButtons(true);
             }
 
         }
@@ -306,17 +489,41 @@ namespace WindowsFormsApp1
 
         private void findIntersectionButton_Click(object sender, EventArgs e)
         {
+            tempEdge.Clear();
+            mode = Mode.Find_Intersection;
+            clearButton.Enabled = false;
+            SetModeButtons(false);
+            outputTextBox.Text = "Постройте 2 отрезка.";
+        }
 
+        int Interpolation(int x0, int y0, int x1, int y1, int x)
+        {
+            return (int)(y0 + (float)(y1 - y0) * (x - x0) / (x1 - x0));
         }
 
         private void convexityCheckButton_Click(object sender, EventArgs e)
         {
-
+            if (polygons.Count > 0)
+            { 
+                mode = Mode.Convexity_Check;
+                clearButton.Enabled = false;
+                SetModeButtons(false);
+                outputTextBox.Text = "Выберите точку.";
+            }
+            else
+            {
+                outputTextBox.Text = "Нет полигонов";
+            }
         }
 
         private void positionRelativeToEdgeButton_Click(object sender, EventArgs e)
         {
+            mode = Mode.Position_Relative_To_Edge;
+            tempEdge.Clear();
+            clearButton.Enabled = false;
+            SetModeButtons(false);
 
+            outputTextBox.Text = "Постройте ребро 2-мя кликами мыши.";
         }
 
         void SetModeButtons(bool enabled)
@@ -382,8 +589,6 @@ namespace WindowsFormsApp1
             pointA = -PointofRotate.X * Math.Cos(angle) + PointofRotate.Y * Math.Sin(angle) + PointofRotate.X;
             pointB = -PointofRotate.X * Math.Sin(angle) - PointofRotate.Y * Math.Cos(angle) + PointofRotate.Y;
 
-
-
             int[] offsetVector = new int[3] { polygonpoint.X, polygonpoint.Y, 1 };
             double[][] Matrix = new double[3][]
             {
@@ -396,27 +601,9 @@ namespace WindowsFormsApp1
         }
         private Point PolygonCentre(List<Point> polygon)
         {
-            int minx= polygon[0].X,maxx= polygon[0].X,miny = polygon[0].Y,maxy = polygon[0].Y;
-            for (int i = 0; i < polygon.Count - 1; i++)
-            {
-                if (polygon[i].X< minx)
-                {
-                    minx = polygon[i].X;
-                }
-                if (polygon[i].X>maxx)
-                {
-                    maxx=polygon[i].X;
-                }
-                if (polygon[i].Y<miny)
-                {
-                    miny= polygon[i].Y;
-                }
-                if (polygon[i].Y>maxy)
-                {
-                    maxy = polygon[i].Y;
-                }
-            }
-            return new Point((maxx + minx) / 2, (maxy + miny) / 2);
+            int sumX = 0, sumY = 0;
+            foreach (Point p in polygon) { sumX += p.X; sumY += p.Y; }
+            return new Point(sumX / polygon.Count, sumY / polygon.Count);
         }
     }
 }
